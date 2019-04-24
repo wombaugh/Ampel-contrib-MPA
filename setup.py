0c1a78ad6da6a9a3ad571ab9301ac7ca713a93da
wombaugh <jnordin@lbl.gov>
2019-04-24 16:16:24 +0200

    notebook

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='Ampel-contrib-MPA',
+      version='0.5.0',
+      package_data = {'': ['*.json']},
+      packages=[
+          'ampel.contrib.mpa.t0',
+      ],
+)
